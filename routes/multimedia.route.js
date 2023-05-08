be5f74d9e224114679bf08fc85931c234e2ce00b
octi14 <gonzalezoctavio5@gmail.com>
2023-05-08 02:08:39 -0300

    Agregado repositorio de archivos multimedia con links

--- routes/multimedia.route.js
+++ routes/multimedia.route.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const MultimediaController = require("../controllers/multimedia.controller");
-const MultimediaValidator = require("../validators/multimedia.validator");
+// const MultimediaValidator = require("../validators/multimedia.validator");
 
 // "/multimedias" endpoints
 router.get("/", MultimediaController.getMultimedias);
-router.post("/", MultimediaValidator.create, MultimediaController.add);
+router.post("/", MultimediaController.add);
 // router.put("/:name", MultimediaController.update);
 router.delete("/:name", MultimediaController.delete);
 // router.get("/:name", MultimediaController.getByName);
