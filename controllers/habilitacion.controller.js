9c5f6f874c98898f94e86cf0164f561808808616
octi14 <gonzalezoctavio5@gmail.com>
2024-12-24 09:29:02 -0300

    Migración de archivos de mongodb Atlas a amazon s3

--- controllers/habilitacion.controller.js
+++ controllers/habilitacion.controller.js
@@ -8,14 +8,13 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
 const AWS = require('aws-sdk');
-const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = require('../config');
 
 
 // Configurar AWS S3
 const s3 = new AWS.S3({
-  accessKeyId: AWS_ACCESS_KEY_ID,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  region: AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 // Crear una nueva instancia de GridFsStorage con la conexión de Mongoose
